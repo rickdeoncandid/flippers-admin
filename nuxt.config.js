90764fdf9e69e328127f4f1120508334a9090870
Sahil Yadav <sahil.yadav21@yahoo.com>
2023-03-30 17:56:26 +0530

    updating flippers api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'https://flippers.club/api/admin',
+    baseURL: 'https://api.flippers.club/',
   },
   /*
    ** vuetify module configuration
