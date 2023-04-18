feec5d50b48e51b614cf0838edbbb8f9d55dee50
Sahil Yadav <sahil.yadav21@yahoo.com>
2023-04-18 19:19:58 +0530

    final admin code push

---
+++ plugins/toast.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueToast from 'vue-toast-notification'
+// Import one of the available themes
+import 'vue-toast-notification/dist/theme-bootstrap.css'
+
+Vue.use(VueToast)
